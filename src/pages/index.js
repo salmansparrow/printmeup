262f73eb8278370678388c32dda6415e91b7f639
salman-mangotech <salman.mangotech7@gmail.com>
2024-09-15 02:30:19 +0500

    white space right side on small screen is set

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ function HomePage() {
         <AiSection />
         <Customize />
         <Price />
-        {/* <Carousel /> */}
+        <Carousel />
         <Testimonials />
       </Layout>
     </>
